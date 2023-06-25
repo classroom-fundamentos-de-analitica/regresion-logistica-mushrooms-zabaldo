f1a1994b75ea34a717d26bdb03f127d5992dbb6e
david <david@debian-BULLSEYE-live-builder-AMD64>
2023-06-25 00:11:28 -0500

    r

--- preguntas.py
+++ preguntas.py
@@ -160,12 +160,12 @@ def pregunta_04():
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
     cfm_train = confusion_matrix(
         y_true=y_train,
-        y_pred=gridSearchCV.predict(X_train),
+        y_pred=pipeline.predict(X_train),
     )
 
     cfm_test = confusion_matrix(
         y_true=y_test,
-        y_pred=gridSearchCV.predict(X_test),
+        y_pred=pipeline.predict(X_test),
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
